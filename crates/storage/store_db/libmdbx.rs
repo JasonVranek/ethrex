403ca85e99141e04a8fcb0338d14511569405d65
fmoletta <fedemoletta@hotmail.com>
2025-06-27 18:00:43 -0300

    Remove dbg

--- crates/storage/store_db/libmdbx.rs
+++ crates/storage/store_db/libmdbx.rs
@@ -80,38 +80,6 @@ impl Store {
         .map_err(|e| StoreError::Custom(format!("task panicked: {e}")))?
     }
 
-    // Helper method to write into a libmdbx table in batch
-    async fn write_batch_loud<T: Table>(
-        &self,
-        key_values: Vec<(T::Key, T::Value)>,
-    ) -> Result<(), StoreError> {
-        let db = self.db.clone();
-        tokio::task::spawn_blocking(move || {
-            let start = Instant::now();
-            let txn = db.begin_readwrite().map_err(StoreError::LibmdbxError)?;
-            let create_txn = start.elapsed().as_millis();
-            info!("[write snap account batch] create_txn: {create_txn}ms");
-            let mut cursor = txn.cursor::<T>().map_err(StoreError::LibmdbxError)?;
-            for (key, value) in key_values {
-                cursor
-                    .upsert(key, value)
-                    .map_err(StoreError::LibmdbxError)?;
-            }
-            let upsert = start.elapsed().as_millis() - create_txn;
-            info!("[write snap account batch] upsert: {upsert}ms");
-            txn.commit().map_err(StoreError::LibmdbxError)?;
-            let commit = start.elapsed().as_millis() - create_txn - upsert;
-            info!("[write snap account batch] commit: {commit}ms");
-            info!(
-                "[write snap account batch] full: {}ms",
-                start.elapsed().as_millis()
-            );
-            Ok(())
-        })
-        .await
-        .map_err(|e| StoreError::Custom(format!("task panicked: {e}")))?
-    }
-
     // Helper method to read from a libmdbx table
     async fn read<T: Table>(&self, key: T::Key) -> Result<Option<T::Value>, StoreError> {
         let db = self.db.clone();
@@ -964,7 +932,7 @@ impl StoreEngine for Store {
         account_hashes: Vec<H256>,
         account_states: Vec<AccountState>,
     ) -> Result<(), StoreError> {
-        self.write_batch_loud::<StateSnapShot>(
+        self.write_batch::<StateSnapShot>(
             account_hashes
                 .into_iter()
                 .map(|h| h.into())
@@ -1161,26 +1129,16 @@ impl StoreEngine for Store {
     ) -> Result<(), StoreError> {
         let db = self.db.clone();
         tokio::task::spawn_blocking(move || {
-        let s = Instant::now();
         let txn =db
             .begin_readwrite()
             .map_err(StoreError::LibmdbxError)?;
-        let txn_create = s.elapsed().as_millis();
-        let mut total_upserts = 0;
         for (acc_hash, nodes) in changeset {
-            total_upserts += nodes.len();
             for (hash, node) in nodes {
                 txn.upsert::<StorageTriesNodes>((acc_hash.0, node_hash_to_fixed_size(hash)), node)
                     .map_err(StoreError::LibmdbxError)?;
             }
         }
-        let data_upsert = s.elapsed().as_millis() - txn_create;
         txn.commit().map_err(StoreError::LibmdbxError)?;
-        let total = s.elapsed().as_millis();
-        let txn_commit = total - txn_create - data_upsert;
-        info!(
-            "[apply_storage_trie_changes] put_batch: {total}ms: txn_create: {txn_create}ms; upserts: {total_upserts} in {data_upsert}ms; txn_commit: {txn_commit}ms"
-        );
         Ok(())
     })
     .await
