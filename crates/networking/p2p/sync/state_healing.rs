f1285c0bca8b96d882fdb568510ccbdc5c76ef15
fmoletta <fedemoletta@hotmail.com>
2025-07-04 16:44:26 -0300

    Remove interval for state healing progress

--- crates/networking/p2p/sync/state_healing.rs
+++ crates/networking/p2p/sync/state_healing.rs
@@ -52,7 +52,7 @@ pub(crate) async fn heal_state_trie(
     paths.push(Nibbles::default());
     let mut last_update = Instant::now();
     while !paths.is_empty() {
-        if last_update.elapsed() >= SHOW_PROGRESS_INTERVAL_DURATION {
+        if /*last_update.elapsed() >= SHOW_PROGRESS_INTERVAL_DURATION*/ true {
             last_update = Instant::now();
             let speed = healing_start.elapsed().as_millis() / total_healed as u128;
             info!(
